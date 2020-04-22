236e3f1ce53cd93346452a0035ae3c9323e06154
msedalatzadeh <msedalatzadeh@math.tu-chemnitz.de>
2020-04-22 14:35:40 +0200

    new changes

--- initial_temp.py
+++ initial_temp.py
@@ -3,12 +3,12 @@ import os
 import matplotlib.pyplot as plt
 
 x=np.arange(0,1,0.01)
-u=8*np.sin(np.pi*x)
+u=10*np.sin(np.pi*x)
 
 plt.xlabel('$x$')
 plt.ylabel('$u(x,0)$')
-plt.axis([0,1,0,9])
-plt.text(0.45, 8.5,'$u_0=8$')
+plt.axis([0,1,0,11])
+plt.text(0.45, 10.5,'$u_0=10$')
 fig=plt.plot(x,u)
 #plt.show()
 
