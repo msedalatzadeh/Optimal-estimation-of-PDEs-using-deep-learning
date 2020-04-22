c8c12cac38897422103e8fb466c6ac99b61c4767
msedalatzadeh <msedalatzadeh@math.tu-chemnitz.de>
2020-04-22 14:14:08 +0200

    new changes

--- initial_temp.py
+++ initial_temp.py
@@ -9,7 +9,10 @@ plt.xlabel('$x$')
 plt.ylabel('$u(x,0)$')
 plt.axis([0,1,0,9])
 plt.text(0.45, 8.5,'$u_0=8$')
-plt.plot(x,u)
-plt.show()
-os.mkdir('figs/')
-plt.savefig('figs/u0.png')
+fig=plt.plot(x,u)
+#plt.show()
+
+if os.path.isfile("./figs/u0.png"):
+   os.remove("./figs/u0.png")   
+
+plt.savefig("./figs/u0.png")
